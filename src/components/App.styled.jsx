e1458e19ba38370e796e7a3842230807aa5ae1e2
Popoaca Andrei <pandrei58@yahoo.com>
2024-02-04 13:38:06 +0200

    Final result

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const AppStyled = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+`;
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SectionTitle = styled.h2`
+  margin-bottom: 20px;
+  font-family: 'Roboto';
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 2;
+`;
+
+export const SectionWrapper = styled.div`
+  margin-bottom: 20px;
+`;
